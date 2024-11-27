b439dad44c25f8784ebad6d3f0c22fa8d5b6255b
Anas Attoum <anasattoum.12321@gmail.com>
2024-11-27 21:37:38 +0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -55,4 +55,4 @@ export const authOptions = {
 
 export const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST, handler as auth };
+export { handler as GET, handler as POST };
