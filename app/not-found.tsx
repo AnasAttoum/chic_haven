3eeb247afd6b3238754abab5d613746c2b3c908a
Anas Attoum <anasattoum.12321@gmail.com>
2024-12-02 19:02:19 +0300

    Create not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function notFound() {
+  return (
+    <div className='flex justify-center items-center smallPage'>
+        <iframe src='/404.svg' className='w-[50vw] h-[50vh]'/>
+    </div>
+  )
+}
